d2acfbdb57f784fac36d69dd333686525fbedd09
Larissa Lima <larissalimz@icloud.com>
2025-06-06 18:25:26 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,46 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt( document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatório(de, ate);
+
+        while (sorteados.includes(numero)){
+            numero = obterNumeroAleatório(de,ate);
+        }
+        sorteados.push(numero);
+    }
+
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class = "texto__paragrafo"> Números sorteados: ${sorteados}</label>`;
+    alterarStatusBotao();
+}
+
+function obterNumeroAleatório (min, max){
+    return Math.floor (Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add ('container__botao');
+    } else {
+       botao.classList.remove('container__botao');
+       botao.classList.add ('container__botao-desabilitado');       
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class = "texto__paragrafo"> Números sorteados: Nenhum até agora</label>';
+    alterarStatusBotao();
+}
